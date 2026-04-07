27c3c1bca03492c3a0bcf90bdd2cfc0a407bd2fe
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-07 15:51:06 +0300

    1.1

--- scr/main/java/uni/student/WeakStudent.java
+++ scr/main/java/uni/student/WeakStudent.java
@@ -1,7 +1,12 @@
 package uni.student;
 
 public class WeakStudent implements Student {
+
     public String getType() {
         return "WEAK";
+    }
+
+    public String getPower() {
+        return "None";
     }
 }
