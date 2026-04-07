e6a457755f86ff926d8ba9a6a8be50338954a7e0
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-07 15:49:56 +0300

    1.1

--- scr/main/java/uni/student/NerdStudent.java
+++ scr/main/java/uni/student/NerdStudent.java
@@ -1,7 +1,13 @@
 package uni.student;
 
 public class NerdStudent implements Student {
+
     public String getType() {
         return "NERD";
     }
+
+    public String getPower() {
+        return "None";
+    }
+    
 }
