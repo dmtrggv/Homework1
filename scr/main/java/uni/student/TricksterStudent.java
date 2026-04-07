d09dd457af4080d2784dbdd70e3b93371e280cb7
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-07 15:50:40 +0300

    1.1

--- scr/main/java/uni/student/TricksterStudent.java
+++ scr/main/java/uni/student/TricksterStudent.java
@@ -1,7 +1,13 @@
 package uni.student;
 
 public class TricksterStudent implements Student {
+
     public String getType() {
         return "TRICKSTER";
     }
+
+    public String getPower() {
+        return "None";
+    }
+    
 }
