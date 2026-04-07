c45dcc4ca61fcf16ea2fc55251e1f7444a16defe
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-07 15:56:16 +0300

    1.1

--- scr/main/java/uni/chain/TricksterHandler.java
+++ scr/main/java/uni/chain/TricksterHandler.java
@@ -4,11 +4,13 @@ import uni.student.Student;
 
 public class TricksterHandler extends Handler {
 
+    @Override
     public void handle(Student student) {
-        if (student.getType().contains("TRICKSTER")) {
+        if (student.getType().equals("TRICKSTER")) {
             System.out.println("Checked in TRICKSTER pile");
         } else if (next != null) {
             next.handle(student);
         }
     }
+    
 }
