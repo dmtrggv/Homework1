ae7819e849f8c20aea2e19d0f8c546e297a07638
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-07 15:55:03 +0300

    1.1

--- scr/main/java/uni/chain/NerdHandler.java
+++ scr/main/java/uni/chain/NerdHandler.java
@@ -4,11 +4,13 @@ import uni.student.Student;
 
 public class NerdHandler extends Handler {
 
+    @Override
     public void handle(Student student) {
-        if (student.getType().contains("NERD")) {
+        if (student.getType().equals("NERD")) {
             System.out.println("Checked in NERD pile");
         } else if (next != null) {
             next.handle(student);
         }
     }
+    
 }
