bec3d8efb4c158a8380fbc747ad39e249b92d0ad
Dimitar Gogov <dmtrkggv@gmail.com>
2026-04-08 11:26:55 +0300

    1.2

--- scr/main/java/uni/task/TaskFactory.java
+++ scr/main/java/uni/task/TaskFactory.java
@@ -6,14 +6,10 @@ public class TaskFactory {
 
     public static Task createTask(Student student) {
         switch (student.getType()) {
-            case "NERD":
-                return new MisleadingTask();
-            case "TRICKSTER":
-                return new ShortTask();
-            case "WEAK":
-                return new OldTask();
-            default:
-                return null;
+            case "NERD": return new MisleadingTask();
+            case "TRICKSTER": return new ShortTask();
+            case "WEAK": return new OldTask();
+            default: return null;
         }
     }
 }
